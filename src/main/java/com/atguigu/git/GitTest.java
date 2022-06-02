024b2fe0a362fcd097f4af423b2babbf7329c5f1
cc <ccoo0107@163.com>
2022-06-02 10:43:23 +0800

    新建

---
+++ src/main/java/com/atguigu/git/GitTest.java
@@ -0,0 +1,16 @@
+package com.atguigu.git;
+
+/**
+ * Date : 2022/6/2
+ * Author : cc
+ * Description :
+ */
+public class GitTest {
+
+    public static void main(String[] args) {
+
+        System.out.println("新建");
+
+    }
+
+}
